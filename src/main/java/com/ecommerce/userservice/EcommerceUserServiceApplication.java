e09f201cdfeef4ee77069eff46fa2a324d3bacbc
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2025-08-13 10:27:55 +0530

    Create EcommerceUserServiceApplication.java

---
+++ src/main/java/com/ecommerce/userservice/EcommerceUserServiceApplication.java
@@ -0,0 +1,11 @@
+package com.yourcompany.ecommerce.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceUserServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceUserServiceApplication.class, args);
+    }
+}
