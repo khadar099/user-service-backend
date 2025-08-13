f406046e4a54fd865ed64870d4e93045c9cc9bd0
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2025-08-13 10:37:45 +0530

    Create UserService.java

---
+++ src/main/java/com/ecommerce/userservice/service/UserService.java
@@ -0,0 +1,35 @@
+package com.yourcompany.ecommerce.userservice.service;
+
+import com.yourcompany.ecommerce.userservice.entity.User;
+import com.yourcompany.ecommerce.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User register(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public User login(String username, String password) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        if (passwordEncoder.matches(password, user.getPassword())) {
+            return user;
+        }
+
+        throw new BadCredentialsException("Invalid password");
+    }
+}
